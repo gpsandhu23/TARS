da51242193b48123dc9d54657df4f5ab1f3621fa
gpsandhu23 <gpsandhu23@gmail.com>
2023-12-30 15:54:48 -0800

    added ai message handler

--- TARS/AI_module/tools.py
+++ TARS/AI_module/tools.py
@@ -1,7 +1,88 @@
+import json
+from dotenv import load_dotenv
 from langchain.agents import tool
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
+# Load environment variables from .env file
+load_dotenv()
 # Create a simple custom tool to test the agent
 @tool
 def get_word_length(word: str) -> int:
     """Returns the length of a word."""
     return len(word)
+
+handle_message_schema = [
+            {
+                "name": "handle_message",
+                "description": "You are an AI named TARS created by GP (Gurpartap Sandhu) to help handle incoming messages",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "summary": {
+                            "description": """
+                            Summary of the message
+                            """,
+                        },
+                        "action_needed": {
+                            "type": "boolean",
+                            "description": "Does this message warrany any action from me",
+                        },
+                        "urgency": {
+                            "type": "string",
+                            "description": """
+                            What is the level of urgency of taking action based on this message.
+                            If I need to do something urgently, it should be high
+                            If I need to do something soon, it should be medium
+                            If I need to eventually do something, it should be low
+                            """,
+                            "enum": ["high", "medium", "low", "unknown"]
+                        },
+                        "importance": {
+                            "type": "string",
+                            "description": """
+                            What is the level of importance for this message.
+                            """,
+                            "enum": ["high", "medium", "low", "unknown"]
+                        },
+                        "action": {
+                            "type": "string",
+                            "description": "What action should be performed, if any",
+                        },
+                        "action_instructions": {
+                            "type": "string",
+                            "description": "What are the step by step instructions to perform the recommended action, if any",
+                        },
+                    },
+                    "required": ["summary", "action_needed", "urgency", "importance", "action", "action_instructions"],
+                }
+            }
+        ]
+
+def ai_email_handler(classifier_input):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}")
+        ]
+    )
+    model = ChatOpenAI(model = "gpt-4-1106-preview", temperature=0).bind(functions=handle_message_schema)
+    runnable = prompt | model
+    # Invoke the runnable with the classifier input
+    message_action = runnable.invoke({"input": classifier_input})
+    content = message_action.content
+    function_call = message_action.additional_kwargs.get('function_call')
+
+    # Check if there's a function call and adjust content accordingly
+    if function_call is not None:
+        content = function_call.get('arguments', content)
+
+    # Convert the string to a JSON object
+    try:
+        json_object = json.loads(content)
+    except json.JSONDecodeError:
+        print("Failed to parse JSON. Here's the raw content:")
+        return content
+
+    # Pretty print the JSON object
+    # print(json.dumps(json_object, indent=4))
+    return json_object
