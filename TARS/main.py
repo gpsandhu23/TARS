b57f5a1a22bfba8f9851b59f8b280142377b670e
gpsandhu23 <gpsandhu23@gmail.com>
2023-12-30 21:39:34 -0800

    turn this into a Slack app DM workflow

--- TARS/main.py
+++ TARS/main.py
@@ -1,18 +1,34 @@
 from AI_module.agent import process_user_task
+from dotenv import load_dotenv
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# Initialize chat_history
+# Load environment variables
+load_dotenv()
+
+# Initialize the chat_history
 chat_history = []
 
-while True:
-    # Get user input
-    user_task = input("Enter your query (or type 'exit' to quit): ")
-    
-    # Check if the user wants to exit
-    if user_task.lower() == 'exit':
-        break
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
 
-    # Call the function from agent.py and pass chat_history
-    agent_response = process_user_task(user_task, chat_history)
-    
-    # Display the result
-    print("Agent Response:", agent_response)
+@app.event("message")
+def message_handler(event, say, ack):
+    ack()
+    print("Message received", event)
+    # Check if the message is a DM and not from a bot
+    if 'channel_type' in event and event['channel_type'] == 'im' and 'bot_id' not in event:
+        message_text = event['text']
+
+        # Call the process_user_task function and pass the message text and the current chat history
+        agent_response_text = process_user_task(message_text, chat_history)
+
+        # Send the response back to the user in Slack
+        say(agent_response_text)
+
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+    handler.start()
