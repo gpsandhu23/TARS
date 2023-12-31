3db6856c911fce8b2f2f01f125e31bdfea5e6f39
gpsandhu23 <gpsandhu23@gmail.com>
2023-12-30 22:44:31 -0800

    added user name to the message to the agent

--- TARS/main.py
+++ TARS/main.py
@@ -16,15 +16,26 @@ app = App(
 )
 
 @app.event("message")
-def message_handler(event, say, ack):
+def message_handler(event, say, ack, client):
     ack()
     print("Message received", event)
     # Check if the message is a DM and not from a bot
     if 'channel_type' in event and event['channel_type'] == 'im' and 'bot_id' not in event:
+        user_id = event['user']
         message_text = event['text']
+        
+        # Fetch user info from Slack API
+        user_info = client.users_info(user=user_id)
+        
+        # Check if the request was successful
+        if user_info and user_info['ok']:
+            user_real_name = user_info['user']['real_name']
+        else:
+            user_real_name = "Unknown"
 
+        agent_input = str({'user name': user_real_name, 'message': message_text})
         # Call the process_user_task function and pass the message text and the current chat history
-        agent_response_text = process_user_task(message_text, chat_history)
+        agent_response_text = process_user_task(agent_input, chat_history)
 
         # Send the response back to the user in Slack
         say(agent_response_text)
